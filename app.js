dfcb9c9a21c17ef45826949f114f9846845897f9
ThinkLee <dinhlethinh137@gmail.com>
2023-10-29 22:53:10 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-alert("Haven't been done yet!! HEHE \nXem trên máy tính thôi nhé <3")
+// alert("Haven't been done yet!! HEHE \nXem trên máy tính thôi nhé <3")
 
 let elToShow = document.querySelectorAll('.show-on-scroll')
 
